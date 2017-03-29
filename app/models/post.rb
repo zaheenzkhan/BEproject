758529d2c8704233e335e32531f5e62a6e6e9c20
Zaheen Khan <zaheenzkhan@gmail.com>
2017-03-29 21:09:46 +0530

    delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+	has_many :comments , dependant: :destroy
 	validates :title , presence: true, length: {minimum: 5}
 	validates :body , presence: true  
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-	resources :posts
+	resources :posts do
+		resources :comments
+	end
+
 	root "posts#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
